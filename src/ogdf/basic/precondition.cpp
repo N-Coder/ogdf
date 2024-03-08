637d261ad6e01ceecb36cca25449a2a9c00c78d5
Simon D Fink <finksim@fim.uni-passau.de>
2024-03-08 09:29:31 +0100

    fix style

--- src/ogdf/basic/precondition.cpp
+++ src/ogdf/basic/precondition.cpp
@@ -1,3 +1,38 @@
+/** \file
+ * \brief Declaration of functions for drawing module precondition
+ *        handling.
+ *
+ * \author Karsten Klein
+ *
+ * \attention This is legacy code from UML class diagram handling,
+ * and it should be checked if it is still required.
+ *
+ * \par License:
+ * This file is part of the Open Graph Drawing Framework (OGDF).
+ *
+ * \par
+ * Copyright (C)<br>
+ * See README.md in the OGDF root directory for details.
+ *
+ * \par
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * Version 2 or 3 as published by the Free Software Foundation;
+ * see the file LICENSE.txt included in the packaging of this file
+ * for details.
+ *
+ * \par
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * \par
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, see
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
 #include <ogdf/basic/precondition.h>
 
 bool ogdf::dfsGenTreeRec(ogdf::UMLGraph& UG, ogdf::EdgeArray<bool, true>& used,
