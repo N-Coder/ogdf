ae3c855902519d5291330cc1fe34c822c88dfd76
Simon D Fink <finksim@fim.uni-passau.de>
2024-03-26 08:16:30 +0100

    better logging in prune caches

--- .github/prune-caches.sh
+++ .github/prune-caches.sh
@@ -3,11 +3,11 @@
 caches=$(gh cache list --key "$1" --ref "$GITHUB_REF" --repo "$GITHUB_REPOSITORY" --sort created_at --order desc --json id --jq ".[].id")
 count=$(echo "$caches" | grep -v ^$ | wc -l)
 if [ "$count" -eq 0 ]; then
-    echo "Found no caches to prune."
+    echo "Found no caches for $GITHUB_REPOSITORY:$GITHUB_REF:$1 to prune."
 elif [ "$count" -eq 1 ]; then
-    echo "Found 1 cache entry ($caches), keeping it."
+    echo "Found 1 cache entry ($caches) for $GITHUB_REPOSITORY:$GITHUB_REF:$1, keeping it."
 else
-    echo "Found $count cache entries:"
+    echo "Found $count cache entries for $GITHUB_REPOSITORY:$GITHUB_REF:$1:"
     echo $caches
     echo "Only retaining the latest (first) one."
     echo "$caches" | tail -n +2 | xargs -L 1 -P 4 --no-run-if-empty --verbose -exec gh cache delete
